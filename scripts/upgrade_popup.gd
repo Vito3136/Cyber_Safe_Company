db30b5bfdf046642185f9eff35a55b5ae1b0c81e
Giuseppe Gentile <giuseppe.gentile81301@gmail.com>
2026-02-19 09:33:01 +0100

    Prices
    Upgrades prices added

--- scripts/upgrade_popup.gd
+++ scripts/upgrade_popup.gd
@@ -66,7 +66,6 @@ func open_popup():
 	show()
 
 func refresh_popup():
-	
 	var purchased_ids = []
 	if (Global.siem_team):
 		purchased_ids.append("siem_team")
@@ -82,17 +81,17 @@ func refresh_popup():
 			var a_unlocked = a.required_id == "" or a.required_id in purchased_ids
 			var b_unlocked = b.required_id == "" or b.required_id in purchased_ids
 			
-			# Se 'a' è sbloccato e 'b' no, 'a' deve venire prima (true)
 			if a_unlocked and not b_unlocked:
 				return true
-			# Se 'b' è sbloccato e 'a' no, 'b' deve venire prima (false)
 			elif not a_unlocked and b_unlocked:
 				return false
-			# Se sono entrambi sbloccati o entrambi bloccati, mantieni l'ordine originale (o usa il titolo)
-			else:
-				var index_a = Global.server_room_available_upgrades.find(a)
-				var index_b = Global.server_room_available_upgrades.find(b)
-				return index_a < index_b
+		
+			if a.costo != b.costo:
+				return a.costo < b.costo
+			
+			var index_a = Global.server_room_available_upgrades.find(a)
+			var index_b = Global.server_room_available_upgrades.find(b)
+			return index_a < index_b
 		)
 		
 		popola_lista(performed_upgrades_container, Global.server_room_performed_upgrades, true, purchased_ids)
@@ -108,17 +107,17 @@ func refresh_popup():
 			var a_unlocked = a.required_id == "" or a.required_id in purchased_ids
 			var b_unlocked = b.required_id == "" or b.required_id in purchased_ids
 			
-			# Se 'a' è sbloccato e 'b' no, 'a' deve venire prima (true)
 			if a_unlocked and not b_unlocked:
 				return true
-			# Se 'b' è sbloccato e 'a' no, 'b' deve venire prima (false)
 			elif not a_unlocked and b_unlocked:
 				return false
-			# Se sono entrambi sbloccati o entrambi bloccati, mantieni l'ordine originale (o usa il titolo)
-			else:
-				var index_a = Global.office_available_upgrades.find(a)
-				var index_b = Global.office_available_upgrades.find(b)
-				return index_a < index_b
+		
+			if a.costo != b.costo:
+				return a.costo < b.costo
+			
+			var index_a = Global.office_available_upgrades.find(a)
+			var index_b = Global.office_available_upgrades.find(b)
+			return index_a < index_b
 		)
 		
 		popola_lista(performed_upgrades_container, Global.office_performed_upgrades, true, purchased_ids)
@@ -134,17 +133,17 @@ func refresh_popup():
 			var a_unlocked = a.required_id == "" or a.required_id in purchased_ids
 			var b_unlocked = b.required_id == "" or b.required_id in purchased_ids
 			
-			# Se 'a' è sbloccato e 'b' no, 'a' deve venire prima (true)
 			if a_unlocked and not b_unlocked:
 				return true
-			# Se 'b' è sbloccato e 'a' no, 'b' deve venire prima (false)
 			elif not a_unlocked and b_unlocked:
 				return false
-			# Se sono entrambi sbloccati o entrambi bloccati, mantieni l'ordine originale (o usa il titolo)
-			else:
-				var index_a = Global.siem_room_available_upgrades.find(a)
-				var index_b = Global.siem_room_available_upgrades.find(b)
-				return index_a < index_b
+		
+			if a.costo != b.costo:
+				return a.costo < b.costo
+			
+			var index_a = Global.siem_room_available_upgrades.find(a)
+			var index_b = Global.siem_room_available_upgrades.find(b)
+			return index_a < index_b
 		)
 		
 		popola_lista(performed_upgrades_container, Global.siem_room_performed_upgrades, true, purchased_ids)
@@ -206,6 +205,7 @@ func _on_bought_upgrade(data: CardData):
 		
 		# 4. Aggiorna tutto
 		refresh_popup()
+		Global.update_monete.emit(Global.monete)
 		Global.upgrade_sicurezza()
 		print("Acquisto effettuato! Monete rimanenti: ", Global.monete)
 	else:
